d9612384319f9c203b60c5afb01a935b020e157e
Young-YKStudio <young@ykstudio.dev>
2022-05-29 12:42:50 -0400

    added key to env

--- src/pages/order/PaymentSetting.js
+++ src/pages/order/PaymentSetting.js
@@ -14,7 +14,7 @@ import { Grid, Typography, Card, CircularProgress } from '@mui/material';
 const PaymentSetting = (props) => {
 
   const [ orderData, setOrderData ] = useState();
-  const [ stripePromise, setStripePromise ] = useState(() => loadStripe('pk_live_51KlJjkKrjDamwGGy9AWU7HoOFRwI3Sj9eXtysjpEuLDhsPEoWMdgtdZ5SdjMt1haq27H8G7vQ1JwHve4SWmYhpm400gBViFosg'))
+  const [ stripePromise, setStripePromise ] = useState(() => loadStripe(`${process.env.STRIPE_KEY}`))
 
   // handlers
 
