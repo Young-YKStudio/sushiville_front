8a4e179ea80057b832c264cf8ff1dc80df0f0c7d
Young-YKStudio <young@ykstudio.dev>
2022-06-06 19:53:45 -0400

    added callback function13

--- src/pages/admin/Dashboard/OrderStatus.js
+++ src/pages/admin/Dashboard/OrderStatus.js
@@ -28,11 +28,7 @@ const OrderStatus = (props) => {
   const [ modalResValue, setModalResValue ] = useState('');
   const [ modalResOpen, setModalResOpen ] = useState(false);
 
-  const socket = io('https://sushiville-socket.herokuapp.com/', {
-    withCredentials: true,
-    transports: ['websocket'],
-    secure: true,
-  })
+  const socket = io('https://sushiville-socket.herokuapp.com/')
   // const socket = io('http://localhost:4000/')
   const audio = new Audio('/sound/ding.mp3')
   let interval
